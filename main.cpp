7ef91bf88abe7e4091f3dbe1275a2d06da5c32a5
NhatTai20 <56631925+NhatTai20@users.noreply.github.com>
2019-11-07 15:24:15 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include"Menu.h"
+#include"DinhThuc.h"
+#include"HePhuongTrinh.h"
+#include"Matrix.h"
+#include"Multiply.h"
+#include"Vector.h"
+#include"Bool.h"
+int main()
+{
+	Menu();
+	return 0;
+}
